fb191b34dd9e8149db63789045fa63347777060d
Sujal Bhavsar <sujalbhavsar16@gmail.com>
2020-05-06 20:11:21 -0400

    Integrate explanatory variable with n-beat

--- sujallvnbeat.py
+++ sujallvnbeat.py
@@ -18,6 +18,11 @@ from numpy import linalg as LA
 from statsmodels.tsa.arima_model import ARIMA
 from pandas import DataFrame
 from pandas import concat
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.stattools import adfuller
+
+
+
 plt.style.use(['amtlvt'])
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=False):
 
@@ -57,6 +62,77 @@ path2='sales_train_validation.csv'
 
 data = pd.read_csv(os.path.join(path1,path2), delimiter=",")
 df = levels.level_3(data)
+df=df.transpose()
+df=pd.DataFrame(df['CA_1'])
+df['d']=df.index
+
+path3='calendar.csv'
+calender=pd.read_csv(os.path.join(path1,path3))     
+store_level_final = df.merge(calender, on='d')
+
+CAL_DTYPES={"event_name_1": "category", "event_name_2": "category", "event_type_1": "category", 
+         "event_type_2": "category", "weekday": "category", 'wm_yr_wk': 'int16', "wday": "int16",
+        "month": "int16", "year": "int16", "snap_CA": "float32", 'snap_TX': 'float32', 'snap_WI': 'float32' }
+ 
+
+for col_name, col_fit in CAL_DTYPES.items():
+        if col_name in store_level_final.columns:
+            store_level_final[col_name] = store_level_final[col_name].astype(col_fit)
+
+for col_name, col_fit in CAL_DTYPES.items():
+    if col_fit =='category':
+        store_level_final[col_name]=store_level_final[col_name].cat.codes.astype('int16')
+        store_level_final[col_name]-=store_level_final[col_name].min()
+
+
+new_store_level=store_level_final.drop(['d','date'],axis=1)
+mw=5
+
+rmse=[]
+mape=[]
+# model prediction with explanatory variables
+nsl_sts=series_to_supervised(new_store_level,5,1)
+nsl_sts=nsl_sts.dropna(axis=0)
+
+t_column=[i for i in nsl_sts.columns if '(t)' in i]+['CA_1(t-5)','CA_1(t-4)','CA_1(t-3)','CA_1(t-2)','CA_1(t-1)']
+exo_column=list(set(list(nsl_sts.columns))-set(t_column))
+input_column=['CA_1(t-5)','CA_1(t-4)','CA_1(t-3)','CA_1(t-2)','CA_1(t-1)']
+output_column='CA_1(t)'
+x=nsl_sts[input_column].values.reshape(df.shape[0]-mw,mw,1)              #changed 1 to 0
+y=nsl_sts[output_column].values.reshape(df.shape[0]-mw,1,1)
+e=nsl_sts[exo_column].values.reshape(df.shape[0]-mw,mw,12)
+num_samples, time_steps, input_dim, output_dim,exo = df.shape[0]-mw, mw, 1, 1,12
+#nbeats code
+
+model = NBeatsNet(exo_dim=exo,backcast_length=time_steps, forecast_length=output_dim,stack_types=(NBeatsNet.GENERIC_BLOCK, NBeatsNet.GENERIC_BLOCK), nb_blocks_per_stack=2,thetas_dim=(4, 4), share_weights_in_stack=True, hidden_layer_units=64)
+model.compile_model(loss='mae', learning_rate=1e-5)
+c = num_samples // 10
+x_train, y_train, x_test, y_test,e_train,e_test = x[c:], y[c:], x[:c], y[:c],e[c:],e[:c]
+model.fit([x_train,e_train], y_train, validation_data=([x_test,e_test], y_test), epochs=25, batch_size=128)
+# Save the model for later.
+model.save('n_beats_model.h5')
+
+# Predict on the testing set.
+predictions = model.predict([x_test,e_test])
+print(predictions.shape)
+
+# Load the model.
+# model2 = NBeatsNet.load('n_beats_model.h5')
+#
+# predictions2 = model2.predict([x_test,e_test])
+# np.testing.assert_almost_equal(predictions, predictions2)
+
+rmse.append(LA.norm(predictions.ravel()-y_test[:,0,0],2))
+mape.append(mean_absolute_percentage_error(predictions.ravel(),y_test[:,0,0]))
+
+fig,ax=plt.subplots()
+ax.plot(predictions.ravel(),linestyle='--',label='prediction')
+ax.plot(y_test[:,0,0],label='target')
+ax.legend()
+plt.show()
+#....................................activate from here............................
+
+
 # rmse=[]
 # mape=[]
 # for mw in [10,15,20,25]:
@@ -125,11 +201,36 @@ df = levels.level_3(data)
 # # print(mean_absolute_percentage_error(predictions.ravel(),y_test[:,0,0]))
 #
 
+# p: The number of lag observations included in the model, also called the lag order.
+# d: The number of times that the raw observations are differenced, also called the degree of differencing.
+# q: The size of the moving average window, also called the order of moving average.
 
-model = ARIMA(df.iloc[0], order=(5,1,0))
-model_fit = model.fit(disp=0)
-print(model_fit.summary())
-residuals = DataFrame(model_fit.resid)
-residuals.plot(kind='kde')
-predictions=model_fit.forecast()
-# print(mean_absolute_percentage_error(predictions.ravel(),y_test[:,0,0]))
+# model = ARIMA(df.iloc[0], order=(5,1,0))
+# model_fit = model.fit(disp=0)
+# print(model_fit.summary())
+# residuals = DataFrame(model_fit.resid)
+# residuals.plot(kind='kde')
+# predictions=model_fit.forecast()
+# plot_acf(df.iloc[0].values)
+# # print(mean_absolute_percentage_error(predictions.ravel(),y_test[:,0,0]))
+# plt.plot(model_fit.predict(1,df.iloc[0].shape[0],typ='levels'),label='prediction')
+# plt.plot(df.iloc[0].values,label='target')
+# plt.legend()
+# plt.show()
+
+
+# X = df.iloc[0].values
+# result = adfuller(X)
+# print('ADF Statistic: %f' % result[0])
+# print('p-value: %f' % result[1])
+# print('Critical Values:')
+
+
+
+
+
+
+
+
+
+
