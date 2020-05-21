79dc5624662b345d5e250c23a92784f92ab56806
Sujal Bhavsar <sujalpb@cntrlsrvs.w2k.vt.edu>
2020-05-20 22:45:00 -0400

    with decomposition of time-series

--- sujallvntcn.py
+++ sujallvntcn.py
@@ -10,12 +10,17 @@ import LevelsCreater as lc
 import pandas as pd
 import os
 from numpy import linalg as LA
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from statsmodels.tsa.seasonal import seasonal_decompose
 
-def s_nbeat(level=1,exo=False,node=0,n_in=30,n_out=1,batch_size=128,optimizer='adam', loss='mse',test=200,epochs=100):
+def s_nbeat(level=1,exo=False,node=0,normalized=False,n_in=30,n_out=1,batch_size=100,optimizer='adam', loss='mse',test=200,epochs=100,decomposition=False):
+
     max_node=get_max_node(level)
     if node>(max_node-1):
         print(f'Exceed the limit: Maximum number of node for level {level} is {max_node}')
         return None,None,None
+    print('level %s' %level+':'+ 'node %s' %node)
     levels = lc.LevelsCreater()
     path1 = 'Data'
     path2 = 'sales_train_validation.csv'
@@ -25,6 +30,7 @@ def s_nbeat(level=1,exo=False,node=0,n_in=30,n_out=1,batch_size=128,optimizer='a
 
         df = df.transpose()
         df = pd.DataFrame(df[df.columns[node]])
+        print('shape of a time-series is %s' %{df.shape})
     # df['d'] = df.index
     #
     # path3 = 'calendar.csv'
@@ -32,28 +38,82 @@ def s_nbeat(level=1,exo=False,node=0,n_in=30,n_out=1,batch_size=128,optimizer='a
     # store_level_final = df.merge(calender, on='d')
     # new_store_level=store_level_final.drop(['d','date'],axis=1)
     # mw=30
-        nsl_sts=series_to_supervised(df,n_in,n_out,dropnan=True)
-        x=nsl_sts[nsl_sts.columns[:n_in]].values
-        y=nsl_sts[nsl_sts.columns[-n_out]].values
-        x_reshape=x.reshape((x.shape[0],x.shape[1],1))
-        y_reshape=y.reshape((y.shape[0],1))
+        if decomposition==True:
+            res=seasonal_decompose(df.values, model='additive',period=1)
+            res=[res.resid,res.seasonal,res.trend]
+            ytot=[]
+            for df in res:
+                print('dealing with decompositioin')
+                df=pd.DataFrame(df)
+                nsl_sts = series_to_supervised(df, n_in, n_out, dropnan=True)
+                x = nsl_sts[nsl_sts.columns[:n_in]].values
+                y = nsl_sts[nsl_sts.columns[-n_out:]].values
+                if normalized == True:
+                    scaler = StandardScaler()
+                    x = scaler.fit_transform(x)
 
-        batch_size, timesteps, input_dim = batch_size, n_in, n_out
-        i = Input(batch_shape=(batch_size, timesteps, input_dim))
+                print('input shape to model: %s' % {x.shape})
+                print('output shape to model: %s' % {y.shape})
 
-        o = TCN(return_sequences=False)(i)  # The TCN layers are here.
-        o = Dense(1)(o)
+                x_reshape = x.reshape((x.shape[0], x.shape[1], 1))
+                y_reshape = y.reshape((y.shape[0], 1))
 
-        m = Model(inputs=[i], outputs=[o])
-        m.compile(optimizer, loss)
+                batch_size, timesteps, input_dim = batch_size, n_in, n_out
+                i = Input(batch_shape=(batch_size, timesteps, input_dim))
 
-        tcn_full_summary(m, expand_residual_blocks=False)
-        c = test
-        x_train, y_train, x_test, y_test = x_reshape[:-c], y_reshape[:-c], x_reshape[-c:], y_reshape[-c:]
+                o = TCN(return_sequences=False)(i)  # The TCN layers are here.
+                o = Dense(1)(o)
 
-        m.fit(x_train, y_train, epochs, validation_split=0.2)
+                m = Model(inputs=[i], outputs=[o])
+                m.compile(optimizer, loss)
 
-        y_pred_test=m.predict(x_test)
+                tcn_full_summary(m, expand_residual_blocks=False)
+
+                c = test
+                x_train, y_train, x_test, y_test = x_reshape[:-c], y_reshape[:-c], x_reshape[-c:], y_reshape[-c:]
+                print('training x size: %s' % {x_train.shape})
+                print('training y size: %s' % {y_train.shape})
+                m.fit(x_train, y_train, epochs, validation_split=0.2)
+
+                y_pred_test = np.round(m.predict(x_test))
+                print('shape of y_pred of decomposition : %s' %{y_pred_test.shape})
+                ytot.append(y_pred_test)
+            y_pred_test=np.sum(ytot,axis=0)
+            print('Final shape of y_pred: %s'%{y_pred_test.shape})
+        else:
+
+            nsl_sts=series_to_supervised(df,n_in,n_out,dropnan=True)
+            x=nsl_sts[nsl_sts.columns[:n_in]].values
+            y=nsl_sts[nsl_sts.columns[-n_out:]].values
+            if normalized==True:
+                scaler = StandardScaler()
+                x=scaler.fit_transform(x)
+
+            print('input shape to model: %s' %{x.shape})
+            print('output shape to model: %s' % {y.shape})
+
+            x_reshape=x.reshape((x.shape[0],x.shape[1],1))
+            y_reshape=y.reshape((y.shape[0],1))
+
+            batch_size, timesteps, input_dim = batch_size, n_in, n_out
+            i = Input(batch_shape=(batch_size, timesteps, input_dim))
+
+            o = TCN(return_sequences=False)(i)  # The TCN layers are here.
+            o = Dense(1)(o)
+
+            m = Model(inputs=[i], outputs=[o])
+            m.compile(optimizer, loss)
+
+            tcn_full_summary(m, expand_residual_blocks=False)
+
+
+            c = test
+            x_train, y_train, x_test, y_test = x_reshape[:-c], y_reshape[:-c], x_reshape[-c:], y_reshape[-c:]
+            print('training x size: %s'%{x_train.shape})
+            print('training y size: %s' %{y_train.shape})
+            m.fit(x_train, y_train, epochs, validation_split=0.2)
+
+            y_pred_test=np.round(m.predict(x_test))
 
     return mean_absolute_percentage_error(y_pred_test,y_test),LA.norm(y_pred_test-y_test,2),RMSSE(y_pred_test,y_test,df.values)
 
@@ -87,5 +147,8 @@ def s_nbeat(level=1,exo=False,node=0,n_in=30,n_out=1,batch_size=128,optimizer='a
 #     return x_train, y_train
 
 if __name__ == "__main__":
-    mape=s_nbeat(node=1)
+    mape,rmse,rmsse=s_nbeat(level=3,node=0,normalized=True,decomposition=True)
+    print('MAPE on testing set: %f' %mape)
+    print('RMSE on testing set: %f' %rmse)
+    print('RMSSE on testin set: %f'%rmsse)
 
