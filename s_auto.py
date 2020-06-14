8bdcd45f1247e7cadaa0bbcb2dbfb9c67c9c7b49
Sujal Bhavsar <sujalpb@cntrlsrvs.w2k.vt.edu>
2020-06-13 21:37:41 -0400

    this is perfectly running

--- s_auto.py
+++ s_auto.py
@@ -12,6 +12,46 @@ from nbeats_keras.model import NBeatsNet
 import tensorflow as tf
 
 #..............................activate from here..................
+# TCN=False
+# if TCN == False:
+#     s_atuto = pd.DataFrame(index=['Nbeat'])
+#     case = 1
+#     test = True
+#     if test == True:
+#         level_range = [12]
+#     else:
+#         level_range = np.arange(1, 13)
+#
+#     for level in level_range:
+#
+#         if level == 10:
+#             node_size=set(pd.read_csv('Results/selected_ts_lv10.csv',index_col=0).values.ravel())
+#             # node_size = np.random.choice(list(range(get_max_node(level))), 100)
+#             print('running')
+#         elif level==11:
+#             node_size = set(pd.read_csv('Results/selected_ts_lv11.csv', index_col=0).values.ravel())
+#         elif level==12:
+#             node_size = set(pd.read_csv('Results/selected_ts_lv12.csv', index_col=0).values.ravel())
+#         else:
+#             node_size = list(range(get_max_node(level)))
+#         print('.....................................', node_size, '....................................')
+#         for node in node_size:
+#             start = time.time()
+#             print(level, node)
+#             print('case:', case)
+#             _, _, rmsse_nbeat = s_nbeat(level=level, node=node, normalized=True, decomposition=True, exo=True, n_in=30,
+#                                         n_out=1)
+#             # _, _, rmsse_ntcn = s_tcn(level=level, node=node, normalized=True, decomposition=True, exo=True, n_in=10,
+#             #                          n_out=1)
+#             s_atuto[str(level) + '_' + str(node)] = [rmsse_nbeat]
+#             s_atuto.to_csv('Data/s_auto.csv')
+#             case += 1
+#             end = time.time()
+#             ts = end - start
+#             print('Time spend: %f' % (ts))
+
+
+#............................................#..............................#
 # s_atuto=pd.DataFrame(index=['Nbeat','Ntcn'])
 # case=1
 # test=True
@@ -49,12 +89,13 @@ import tensorflow as tf
 
 n_beat_model=next(os.walk('n_beat_models'))[2]
 # print
+print(len(n_beat_model))
 test=False
 if test==True:
-    length=[0,1]
-    horizon=range(2)
+    length=[0,1,2,3]
+    horizon=range(5)
 else:
-    length=np.arange(len(n_beat_model))
+    length=range(len(n_beat_model))
     horizon=range(28)
 
 s_prediction=pd.DataFrame()
@@ -62,7 +103,8 @@ for i in length:
 
     node=int(n_beat_model[i][:-3].split('_')[-1])
     level=int(n_beat_model[i][:-3].split('_')[-2])
-    print(level)
+    print('Level:',level)
+    print('node:',node)
 
     path1 = 'Data2 '
     path2 = 'sales_train_validation.csv'
@@ -71,17 +113,17 @@ for i in length:
     sale = pd.read_csv('Data2/sales_train_evaluation.csv')
     calendar = pd.read_csv('Data2/calendar.csv')
     price = pd.read_csv('Data2/sell_prices.csv')
-    levels=lc.LevelsCreater()
-    df = levels.get_level(sale, level)
-    df = df.transpose()
-    df = pd.DataFrame(df[df.columns[node]])
+    # levels=lc.LevelsCreater()
+    # df = levels.get_level(sale, level)
+    # df = df.transpose()
+    # df = pd.DataFrame(df[df.columns[node]])
     ex = exovar.exovar()
     #......................update ts_level and run in for loop aha............#
     an=[]
     ts_level = lc.LevelsCreater().get_level(sale, level)
     for h in horizon:
-
-        print(ts_level.shape)
+        print('horizon-',h)
+        # print(ts_level.shape)
         salecal = ex.salecal(ts_level, calendar, node)
         if level==1:
             salecal=salecal.rename(columns={salecal.columns[0]:'zero'})
@@ -94,14 +136,14 @@ for i in length:
         model = NBeatsNet.load(os.path.join('n_beat_models',n_beat_model[0]))
 
         prediction = model.predict([x,e])
-        print(prediction.shape)
-        print(prediction.ravel().shape)
+        # print(prediction.shape)
+        # print(prediction.ravel().shape)
 
         ts_level['d_'+str(int(ts_level.columns[-1].split('_')[1])+1)]=np.zeros(get_max_node(level))
         ts_level.iloc[node,-1]=int(prediction.ravel())
         an.append(int(prediction.ravel()))
     s_prediction[str(level)+'_'+str(node)]=an
 
-print(ts_level)
-print(s_prediction)
+# print(ts_level)
+# print(s_prediction)
 s_prediction.to_csv('Results/s_prediction.csv')
